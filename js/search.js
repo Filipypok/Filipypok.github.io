1a3d6531ccec47fc300cd058e6809347447d1e09
Maks Filipypok <generalov.maks.84@gmail.com>
2025-07-05 15:19:35 +0500

    search and meta

--- js/search.js
+++ js/search.js
@@ -1,57 +1,88 @@
 const productMap = {
-    "блок для сушильной машины": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "вибросито": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "сито": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "сушильная машина": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "сушка": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "блок для сушильной машины": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "вакуумный дегазатор": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "вертикальный шламовый насос": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "вибросито с отрицательным давлением": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "вибросито": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "винтовой насос": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "илоотделитель": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "комната контейнерного типа": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "очиститель раствора": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "пескоотделитель": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "система обработки шлама": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "система обработки глинистой воды": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "срезающий насос": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "сушильная машина": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "центрифуга": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "центробежный шламовый насос": "neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
-    "немецкий тип оси": "trailerAxle/bridgeTrailerPage.html.html",
-    "американский тип оси": "trailerAxle/bridgeTrailerPage.html.html",
-    " ": "trailerAxle/bridgeTrailerPage.html.html",
-    "низкоплатформенный автомобильный мост": "trailerAxle/bridgeTrailerPage.html",
-    "ось дискового тормоза": "trailerAxle/bridgeTrailerPage.html",
-    "тайская ось": "trailerAxle/bridgeTrailerPage.html",
-    "немецкий тип оси": "trailerAxle/bridgeTrailerPage.html",
-    "американский тип оси": "trailerAxle/bridgeTrailerPage.html",
-  };
-
-  // Расстояние Левенштейна
-  function levenshtein(a, b) {
-    const matrix = [];
-    for (let i = 0; i <= b.length; i++) matrix[i] = [i];
-    for (let j = 0; j <= a.length; j++) matrix[0][j] = j;
-
-    for (let i = 1; i <= b.length; i++) {
-      for (let j = 1; j <= a.length; j++) {
-        if (b[i - 1] === a[j - 1]) {
-          matrix[i][j] = matrix[i - 1][j - 1];
-        } else {
-          matrix[i][j] = Math.min(
-            matrix[i - 1][j - 1] + 1,
-            matrix[i][j - 1] + 1,
-            matrix[i - 1][j] + 1
-          );
+    "блок для сушильной машины": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "вибросито": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "сито": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "сушильная машина": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "сушка": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "блок для сушильной машины": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "вакуумный дегазатор": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "вертикальный шламовый насос": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "вибросито с отрицательным давлением": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "вибросито": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "винтовой насос": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "илоотделитель": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "комната контейнерного типа": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "очиститель раствора": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "пескоотделитель": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "система обработки шлама": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "система обработки глинистой воды": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "срезающий насос": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "сушильная машина": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "центрифуга": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "центробежный шламовый насос": "/neftyanoye-oborudovanie/neftyanoye-oborudovanie.html",
+    "немецкий тип оси": "/trailerAxle/bridgeTrailerPage.html.html",
+    "американский тип оси": "/trailerAxle/bridgeTrailerPage.html.html",
+    "низкоплатформенный автомобильный мост": "/trailerAxle/bridgeTrailerPage.html",
+    "ось дискового тормоза": "/trailerAxle/bridgeTrailerPage.html",
+    "тайская ось": "/trailerAxle/bridgeTrailerPage.html",
+    "немецкий тип оси": "/trailerAxle/bridgeTrailerPage.html",
+    "американский тип оси": "/trailerAxle/bridgeTrailerPage.html",
+    "жесткая подвеска": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "установленный светильник": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "единая точка подвески": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "американская подвеска": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "американский тип подвески": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "немецкая подвеска": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "немецкий тип подвески": "/vozdushnaya-podveska/vozdushnaya-podveska.html",
+    "ступица": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "тормозной барабан": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "тормозная колодка": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "суппорт": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "брызговик": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "колпак и уплотняющий элемент": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "датчик ABS и ротор": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "подъемный рычаг": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "привод и комплектные аксессуары": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "тормозная камера": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "комплектный болт": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "пружина": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "подшипник": "/zapchasti-dlya-mosta-gruzovyh-avtomobiley/zapchasti-dlya-mosta-gruzovyh-avtomobiley.html",
+    "обработка тормозного барабана": "/Casting/CastingPage.html",
+    "обработка ступицы колеса": "/Casting/CastingPage.html",
+    "литьё деталей специальной формы": "/Casting/CastingPage.html",
+    "процесс потери пены": "/Casting/CastingPage.html",
+    "литьё под давлением специальной формы": "/Casting/CastingPage.html",
+    "литьё из песка с покрытием": "/Casting/CastingPage.html",
+    "литьё деталей специальной формы": "/Casting/CastingPage.html",
+    "автоматическая машина для литья": "/rezinovye-izdeliya/rezinovye-izdeliya.html",
+    "резиновый экструдер": "/rezinovye-izdeliya/rezinovye-izdeliya.html",
+    "резиновый каландр": "/rezinovye-izdeliya/rezinovye-izdeliya.html",
+    "резиновая плоская вулканизационная машина": "/rezinovye-izdeliya/rezinovye-izdeliya.html",
+    "смеситель": "/rezinovye-izdeliya/rezinovye-izdeliya.html"
+  }; // <-- Закрываем объект productMap
+  
+    // Расстояние Левенштейна
+    function levenshtein(a, b) {
+      const matrix = [];
+      for (let i = 0; i <= b.length; i++) matrix[i] = [i];
+      for (let j = 0; j <= a.length; j++) matrix[0][j] = j;
+  
+      for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+          if (b[i - 1] === a[j - 1]) {
+            matrix[i][j] = matrix[i - 1][j - 1];
+          } else {
+            matrix[i][j] = Math.min(
+              matrix[i - 1][j - 1] + 1,
+              matrix[i][j - 1] + 1,
+              matrix[i - 1][j] + 1
+            );
+          }
         }
       }
-    }
-
-    return matrix[b.length][a.length];
-  }
+  
+      return matrix[b.length][a.length];
+    } 
 
   // Частичное совпадение
   function partialMatch(query, map) {
